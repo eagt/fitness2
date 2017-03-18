8dee5b37dd2e0df5423ff462dcc21962abd485f3
Mario Gamez <mariogamez@DevMm.local>
2017-03-17 22:28:38 -0600

    cambios in exercise

--- db/migrate/20170316110933_create_measurements.rb
+++ db/migrate/20170316110933_create_measurements.rb
@@ -2,7 +2,7 @@ class CreateMeasurements < ActiveRecord::Migration[5.0]
   def change
     create_table :measurements do |t|
       t.integer :user_id
-      t.string :date
+      t.date :date
       t.string :weight
       t.string :weight_unit
       t.string :fat_pct
